19cc683746bc10509db6630148127fd6b0216c7b
minnukota381 <minnukota381@gmail.com>
2024-08-18 09:12:48 +0530

    "Updated PondManagementComponent to use styleUrls instead of styleUrl and added initial ponds data"

--- src/app/pond-management/pond-management.component.ts
+++ src/app/pond-management/pond-management.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-pond-management',
   templateUrl: './pond-management.component.html',
-  styleUrl: './pond-management.component.css'
+  styleUrls: ['./pond-management.component.css']
 })
 export class PondManagementComponent {
-
+  ponds = [
+    { id: 1, name: 'Pond 1', description: 'This is Pond 1' },
+    { id: 2, name: 'Pond 2', description: 'This is Pond 2' },
+    // Add more ponds as needed
+  ];
 }
