c1489eba0de6493341e6744cd909ac2e0dc3f754
minnukota381 <minnukota381@gmail.com>
2024-08-18 10:43:27 +0530

    "Added routes to app-routing.module.ts and modified sensor-details.component.html"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { PondManagementComponent } from './pond-management/pond-management.component';
+import { SensorDetailsComponent } from './sensor-details/sensor-details.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Default route
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'pond-management', component: PondManagementComponent },
+  { path: 'pond/:id', component: SensorDetailsComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: '' } // Wildcard route to redirect to Home if no route matches
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
