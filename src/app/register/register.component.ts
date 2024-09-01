f670d461efda55832fcc610ed8dc2684bad2991b
minnukota381 <minnukota381@gmail.com>
2024-09-01 20:25:21 +0530

    "Added register component HTML and updated TS file with user interface and form submission logic"

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface User {
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  user: User = {
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  };
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    if (this.user.password !== this.user.confirmPassword) {
+      console.error('Passwords do not match');
+      return;
+    }
+    console.log('Form submitted', this.user);
+    this.user = {
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    };
+  }
 }
